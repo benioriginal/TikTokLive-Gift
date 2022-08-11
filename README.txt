130fe96d1370615422a5a66d7682b5af7554e15b
BainBan <111044589+BainBan@users.noreply.github.com>
2022-08-11 03:47:48 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,13 @@
+Made by BainBan :
+
+run : pip install -r requirements.txt
+run "script" put your id from "https://www.tiktok.com/@your_id/live" WITHOUT THE "@"
+you can change the action that it does.U can ask me or search for pynput documentation 
+also you can change the action based on how many gifts they sent by adding this:
+elif event.gift.gift_type != number:
+        print(f"{event.user.uniqueId} sent \"{event.gift.extended_gift.name}\"")
+        (the action you want to do)
+
+change the ,,number'' to how many gifts you want
+Also _Bupi_ or Palarie_Mortala are noobs
+enjoy!
---
+++ script.py
@@ -0,0 +1,69 @@
+from TikTokLive import TikTokLiveClient
+from TikTokLive.types.events import CommentEvent, ConnectEvent
+import pynput
+from pynput.keyboard import Key, Controller
+from tomlkit import key
+keyboard = Controller()
+print("""
+███╗░░░███╗░█████╗░██████╗░███████╗  ██████╗░██╗░░░██╗  ██████╗░░█████╗░██╗███╗░░██╗██████╗░░█████╗░███╗░░██╗
+████╗░████║██╔══██╗██╔══██╗██╔════╝  ██╔══██╗╚██╗░██╔╝  ██╔══██╗██╔══██╗██║████╗░██║██╔══██╗██╔══██╗████╗░██║
+██╔████╔██║███████║██║░░██║█████╗░░  ██████╦╝░╚████╔╝░  ██████╦╝███████║██║██╔██╗██║██████╦╝███████║██╔██╗██║
+██║╚██╔╝██║██╔══██║██║░░██║██╔══╝░░  ██╔══██╗░░╚██╔╝░░  ██╔══██╗██╔══██║██║██║╚████║██╔══██╗██╔══██║██║╚████║
+██║░╚═╝░██║██║░░██║██████╔╝███████╗  ██████╦╝░░░██║░░░  ██████╦╝██║░░██║██║██║░╚███║██████╦╝██║░░██║██║░╚███║
+╚═╝░░░░░╚═╝╚═╝░░╚═╝╚═════╝░╚══════╝  ╚═════╝░░░░╚═╝░░░  ╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝╚═════╝░╚═╝░░╚═╝╚═╝░░╚══╝
+
+░██████╗██╗░░░██╗██████╗░░██████╗░█████╗░██████╗░██╗██████╗░███████╗  ████████╗░█████╗░
+██╔════╝██║░░░██║██╔══██╗██╔════╝██╔══██╗██╔══██╗██║██╔══██╗██╔════╝  ╚══██╔══╝██╔══██╗
+╚█████╗░██║░░░██║██████╦╝╚█████╗░██║░░╚═╝██████╔╝██║██████╦╝█████╗░░  ░░░██║░░░██║░░██║
+░╚═══██╗██║░░░██║██╔══██╗░╚═══██╗██║░░██╗██╔══██╗██║██╔══██╗██╔══╝░░  ░░░██║░░░██║░░██║
+██████╔╝╚██████╔╝██████╦╝██████╔╝╚█████╔╝██║░░██║██║██████╦╝███████╗  ░░░██║░░░╚█████╔╝
+╚═════╝░░╚═════╝░╚═════╝░╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝╚═════╝░╚══════╝  ░░░╚═╝░░░░╚════╝░
+
+██████╗░░█████╗░██╗███╗░░██╗██████╗░░█████╗░███╗░░██╗ 
+██╔══██╗██╔══██╗██║████╗░██║██╔══██╗██╔══██╗████╗░██║ 
+██████╦╝███████║██║██╔██╗██║██████╦╝███████║██╔██╗██║ 
+██╔══██╗██╔══██║██║██║╚████║██╔══██╗██╔══██║██║╚████║ 
+██████╦╝██║░░██║██║██║░╚███║██████╦╝██║░░██║██║░╚███║ 
+╚═════╝░╚═╝░░╚═╝╚═╝╚═╝░░╚══╝╚═════╝░╚═╝░░╚═╝╚═╝░░╚══╝ 
+""")
+name = input("Id ul de pe tt? ")
+
+# Instantiate the client with the user's username
+client: TikTokLiveClient = TikTokLiveClient(unique_id=f"@{name}")
+
+
+# Define how you want to handle specific events via decorator
+
+
+@client.on("connect")
+async def on_connect(_: ConnectEvent):
+    print("Connected to Room ID:", client.room_id)
+
+
+# Notice no decorator?
+async def on_comment(event: CommentEvent):
+    total_donations = 0
+    total_donations += 1
+
+    # Define handling an event via "callback"
+client.add_listener("comment", on_comment)
+
+
+@client.on("gift")
+async def on_gift(event):
+    # If it's type 1 and the streak is over
+    if event.gift.gift_type == 1:
+        if event.gift.repeat_end == 1:
+            print(
+                f"{event.user.uniqueId} sent {event.gift.repeat_count}x \"{event.gift.extended_gift.name}\"")
+            keyboard.press(Key.up)
+            keyboard.release(Key.up)
+    # It's not type 1, which means it can't have a streak & is automatically over
+    elif event.gift.gift_type != 1:
+        print(f"{event.user.uniqueId} sent \"{event.gift.extended_gift.name}\"")
+        keyboard.press(Key.up)
+        keyboard.release(Key.up)
+if __name__ == '__main__':
+    # Run the client and block the main thread
+    # await client.start() to run non-blocking
+    client.run()
